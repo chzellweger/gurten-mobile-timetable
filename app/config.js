7ff1315cdce0e26103801098158834ce86fa2a10
chzellweger <chzellweger@gmail.com>
2016-06-12 15:53:35 +0200

    created config.js

---
+++ app/config.js
@@ -0,0 +1,8 @@
+const config = {
+  // data: set data-source in './app/app.js -> import Data from './...'
+  appRoot: document.getElementById('root'),
+  stages: ['Hauptbühne', 'Zeltbühne', 'Waldbühne'],
+  days: ['do', 'fr', 'sa', 'so']
+};
+
+export default config;
